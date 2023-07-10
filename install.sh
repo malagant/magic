a994608b6b9d34d5a8f121256f1ac794b3ae87f0
Flox User <floxuser@example.invalid>
2023-07-10 13:13:00 +0200

    WIP: More configs

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 FLOX_ENV_NAME=macos
-PACKAGES_TO_INSTALL="git zsh tmux neovim ripgrep rustup home-manager zellij"
+PACKAGES_TO_INSTALL="git direnv zsh tmux neovim ripgrep rustup home-manager zellij zoxide alacritty iterm2 antibody"
 # Download and nstall flox binary
 installFlox() {
 	curl -o flox.aarch64-darwin.pkg 'https://floxdev.com/downloads/osx/flox.aarch64-darwin.pkg'
@@ -12,6 +12,8 @@ installFlox() {
 
 # Setup flox and all
 setupFlox() {
+	nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+        nix-channel --update
 	flox create -e $FLOX_ENV_NAME || true
 	flox install -e $FLOX_ENV_NAME "$PACKAGES_TO_INSTALL"
 }
