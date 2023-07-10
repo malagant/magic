96377615f54bcaa5329216caeb00d70fe5306e44
michael.johann@telekom.de <michael.johann@telekom.de>
2023-07-10 14:02:05 +0200

    setup of home-manager

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 
 FLOX_ENV_NAME=macos
 PACKAGES_TO_INSTALL="git direnv zsh tmux neovim ripgrep rustup home-manager zellij zoxide alacritty iterm2 antibody"
+HOME_NIX="$HOME/.config/home-manager/home.nix"
+
 # Download and nstall flox binary
 installFlox() {
 	curl -o flox.aarch64-darwin.pkg 'https://floxdev.com/downloads/osx/flox.aarch64-darwin.pkg'
@@ -13,13 +15,22 @@ installFlox() {
 # Setup flox and all
 setupFlox() {
 	nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-        nix-channel --update
+	nix-channel --update
 	flox create -e $FLOX_ENV_NAME || true
 	flox install -e $FLOX_ENV_NAME "$PACKAGES_TO_INSTALL"
 }
 
+setupHomeManager() {
+	if [ ! -f "$HOME_NIX" ]; then
+		echo "home.nix not found, creating it"
+		home-manager init
+	fi
+	home-manager switch
+}
+
 activateEnvironment() {
 	flox activate -e $FLOX_ENV_NAME
+
 }
 
 installRust() {
@@ -35,5 +46,7 @@ else
 	setupFlox
 	activateEnvironment
 fi
+echo "Setting up home-manager"
+setupHomeManager
 echo "Installing rust"
 installRust
